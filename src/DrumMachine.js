03f8e5b14e1e9a6d22feb2019218b9d6e8ab315e
António <oantoniomauricio@gmail.com>
2022-12-02 12:07:46 +0000

    DrumMachine comp created

---
+++ src/DrumMachine.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class DrumMachine extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <p>Still pushin P?</p>
+        )
+    }
+}
+
+export default DrumMachine
